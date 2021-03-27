71f74ed3eb2de55bef27e22007d2dd586e36db1f
Riley Boyd <contact@rileyboyd.com>
2021-03-27 15:57:30 -0400

    Adding node config from https://www.gatsbyjs.com/tutorial/wordpress-source-plugin-tutorial/

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,21 @@
+Copygatsby-node.js: copy code to clipboard
 const path = require(`path`)
-// Log out information after a build is done
-exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
-// Create blog pages dynamically
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  const result = await graphql(`
-    query {
-      allSamplePages {
+  return graphql(`
+    {
+      allWpPost(sort: { fields: [date] }) {
         edges {
           node {
-            slug
             title
+            excerpt
+            content
+            slug
           }
         }
       }
     }
-  `)
-  result.data.allSamplePages.edges.forEach(edge => {
-    createPage({
-      path: `${edge.node.slug}`,
-      component: blogPostTemplate,
-      context: {
-        title: edge.node.title,
-      },
-    })
+  `).then(result => {
+    console.log(JSON.stringify(result, null, 4))
   })
 }
