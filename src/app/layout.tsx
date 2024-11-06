5e26f0afcaf739b17ae5ae8acfe3596aed0213bf
Riley Boyd <contact@rileyboyd.com>
2024-11-06 14:21:54 -0500

    Adding SCSS files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@/styles/index.scss";
 
 export const metadata: Metadata = {
   title: "Riley Boyd: Senior Frontend Engineer",
