4d1acae8f5d8fa2145a7dd3b30f94689908ad498
Riley Boyd <contact@rileyboyd.com>
2024-11-06 14:53:36 -0500

    Adding bootstrap and other packages needed for styles from existing site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "@/styles/index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css";
+import "flickity/dist/flickity.min.css";
+import "photoswipe/dist/photoswipe.css";
+import "photoswipe/dist/default-skin/default-skin.css";
 
 export const metadata: Metadata = {
   title: "Riley Boyd: Senior Frontend Engineer",
