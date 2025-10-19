f1d2ad5240f4058cb0acdf899ba1ccc58f157cf4
Riley Boyd <contact@rileyboyd.com>
2025-10-19 02:51:00 -0400

    Making cypress tests wait longer for elements that animate in

--- cypress/e2e/home/home.cy.ts
+++ cypress/e2e/home/home.cy.ts
@@ -3,10 +3,14 @@ describe('Home Page Tests', () => {
     cy.visit('/')
   })
   it('should display the correct introductory text', () => {
-    cy.contains(/Hello, I'm Riley Boyd./).should('be.visible')
+    cy.contains(/Hello, I'm Riley Boyd./, { timeout: 30000 }).should(
+      'be.visible'
+    )
   })
 
   it('should display the "View My Work" button', () => {
-    cy.contains('button', 'View My Work').should('be.visible')
+    cy.contains('button', 'View My Work', { timeout: 30000 }).should(
+      'be.visible'
+    )
   })
 })
