5cd4c9d8103081d5bad2bc6094c7fd2232702bae
Riley Boyd <contact@rileyboyd.com>
2025-10-19 20:08:51 -0400

    Improving home.cy.ts to validate that user can navigate to the Portfolio page from the View My Work button in the Hero section

--- cypress/e2e/home/home.cy.ts
+++ cypress/e2e/home/home.cy.ts
@@ -2,7 +2,18 @@ describe('Home Page Tests', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('should display the "View My Work" button', () => {
+
+  it('should allow the user to navigate to the Portfolio page by clicking the "View My Work" button in the hero section', () => {
+    // Ensure the button exists
     cy.contains('button', 'View My Work').should('exist')
+
+    // Click the button
+    cy.contains('button', 'View My Work').click()
+
+    // Option 1: Check the URL (if you use routing)
+    cy.url().should('include', '/portfolio')
+
+    // Option 2: Check for specific content on the Portfolio page
+    cy.contains('h1', 'Portfolio').should('be.visible')
   })
 })
